42a2435f75b03124d0899585a9bff3e5c7f0de46
Dominic Tarr <dominic.tarr@gmail.com>
2016-10-16 16:14:58 +0100

    parsing CoffHeader, SectionHeader, Symbol

--- parse.js
+++ parse.js
@@ -2,11 +2,11 @@
 var v = require('varstruct')
 
 
-var FileHeader = v([
+var CoffHeader = v([
   {name: 'machine', type: v.UInt16LE},
   {name: 'sections', type: v.UInt16LE},
   {name: 'timestamp', type: v.UInt32LE},
-  {name: 'filePointer', type: v.UInt32LE},
+  {name: 'symbolPointer', type: v.UInt32LE},
   {name: 'symbols', type: v.UInt16LE},
   {name: 'optionalHeaderSize', type: v.UInt32LE},
   {name: 'characteristics', type: v.UInt16LE}
@@ -25,29 +25,77 @@ var SectionHeader = v([
   {name: 'characteristics', type: v.UInt32LE},
 ])
 
+var Symbol = v([
+  {name: 'name',             type: v.Buffer(8)},
+  {name: 'value',            type: v.UInt32LE},
+  {name: 'sectionNumber',    type: v.UInt16LE},
+  {name: 'type',             type: v.UInt16LE},
+  {name: 'storageClass',     type: v.UInt8},
+  {name: 'numberOfAuxSymbols', type: v.UInt8}
+])
+
 module.exports = function (buffer) {
-  FileHeader.decode(buffer, 0)
+  COFFHeader.decode(buffer, 0)
   //if optional header size is zero, parse section header(s)
+}
+
+function parseArray(buffer, n, offset, decoder) {
+  var array = []
+  while(n--) {
+    array.push(decoder(buffer, offset))
+    offset += decoder.bytes
+  }
+  parseArray.bytes = offset
+  return array
+}
+
+function cString(s) {
+  return s.toString('ascii').replace(/\u0000+$/,'') //a cstring (null terminated)
 }
 
 if(!module.parent) {
   var output = {}
   var offset = 0
   var buffer = require('fs').readFileSync('HELLO2.OBJ')
-  var fh = output.fileHeader = FileHeader.decode(buffer)
-  offset+= FileHeader.decode.bytes
-  if(fh.optionalHeaderSize !== 0) throw new Error('optional header not yet implemented')
+  var ch = output.coffHeader = CoffHeader.decode(buffer)
+  offset+= CoffHeader.decode.bytes
+  if(ch.optionalHeaderSize !== 0) throw new Error('optional header not yet implemented')
 
-  var sections = fh.sections
-  output.sections = []
-  while(sections --) {
-    var sh = SectionHeader.decode(buffer, offset)
-    sh.name = sh.name.toString('ascii').replace(/\u0000+$/,'') //a cstring (null terminated)
-    offset += SectionHeader.decode.bytes
-    output.sections.push(sh)
-  }
+  var sections = ch.sections
+//  output.sections = []
+//  while(sections --) {
+//    var sh = SectionHeader.decode(buffer, offset)
+//    sh.name = sh.name.toString('ascii').replace(/\u0000+$/,'') //a cstring (null terminated)
+//    offset += SectionHeader.decode.bytes
+//    sh.data = buffer.slice(sh.pointerToRawData,sh.pointerToRawData+sh.sizeOfRawData) 
+//    output.sections.push(sh)
+//  }
+
+  output.sections =
+    parseArray(buffer, output.coffHeader.sections, offset, SectionHeader.decode)
+    .map(function (sh) {
+      sh.name = cString(sh.name)
+      //the way it reads ahead sort of breaks varstruct's abstraction.
+      sh.data = buffer.slice(sh.pointerToRawData,sh.pointerToRawData+sh.sizeOfRawData)
+
+      //TODO: 1. parse relocations
+      //      2. parse line numbers
+
+      return sh
+    })
+
+  offset = output.coffHeader.symbolPointer
+
+  output.symbols =
+    parseArray(buffer, output.coffHeader.symbols, offset, Symbol.decode)
+    .map(function (e) {
+      //actually a friggen UNION.. "See Section 5.4.1, Symbol Name Representation"
+      e.name = cString(e.name)
+      return e
+    })
 
   console.log(output)
+  console.log(max)
 }
 
 
